dd97ae8927f74933daf5fdf8a69cc90c18040c84
Matt Reinert <matt.reinert@gmail.com>
2023-10-01 13:04:48 -0400

    initial commit

---
+++ src/test/java/mjr/personalfinance/apis/ApisApplicationTests.java
@@ -0,0 +1,13 @@
+package mjr.personalfinance.apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
